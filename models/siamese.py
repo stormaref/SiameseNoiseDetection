abb3d5d7205b56ee6942374fefb6329b8ef58079
“Aref <storm.aref@gmail.com>
2024-08-07 13:54:09 +0330

    resnet50

--- models/siamese.py
+++ models/siamese.py
@@ -1,11 +1,17 @@
 import torch
 import torch.nn as nn
 from torchvision.models import resnet18, ResNet18_Weights
+from torchvision.models import resnet50, ResNet50_Weights
 
 class SiameseNetwork(nn.Module):
-    def __init__(self, num_classes=10):
+    def __init__(self, num_classes=10, model='resnet18'):
         super(SiameseNetwork, self).__init__()
-        base_model = resnet18(weights=ResNet18_Weights.DEFAULT)
+        if model == 'resnet18':
+            base_model = resnet18(weights=ResNet18_Weights.DEFAULT)
+        elif model == 'resnet50':
+            base_model = resnet50(weights=ResNet50_Weights.DEFAULT)
+        else:
+            raise ValueError('Model not supported')
         self.feature_extractor = nn.Sequential(*list(base_model.children())[:-1])
         self.fc_embedding = nn.Linear(512, 128)  # Embedding layer for Siamese Network
         self.fc_classifier = nn.Linear(128, num_classes)  # Classifier layer
