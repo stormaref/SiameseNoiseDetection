be5cd7385bdb672186d6a7c5ed0af0e5f99f36e3
stormaref <storm.aref@gmail.com>
2024-08-24 01:27:36 +0330

    idk

--- models/predictor.py
+++ models/predictor.py
@@ -1,35 +1,44 @@
+import importlib.readers
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+import importlib
+from models import resnet
+importlib.reload(resnet)
 from models.resnet import Resnet
 from models.dataset import CustomDataset
 import numpy as np
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Subset
+from torchvision import transforms
 
 class Predictor:
-    def __init__(self, train_dataset, test_dataset, transform, num_classes=10, model_type='resnet18', batch_size=256, device=None):
-        train_dataset, val_dataset = self.get_train_val_test_datasets(train_dataset)
-        self.train_dataset = CustomDataset(train_dataset, transform)
-        self.val_dataset = CustomDataset(val_dataset, transform)
-        self.test_dataset = CustomDataset(test_dataset, transform)
-        self.batch_size = batch_size
+    def __init__(self, train_dataset, test_dataset, transform, num_classes=10, model_type='resnet18', batch_size=256, 
+                 device=None, use_validation=False, pre_trained=False, weight_decay=0.001):
+        self.use_validation = use_validation
         self.device = device if device else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        test_transform = transforms.Compose([transforms.ToTensor()])
+        if use_validation:
+            train_dataset, val_dataset = self.get_train_val_datasets(train_dataset)
+            self.val_dataset = CustomDataset(val_dataset, test_transform)
+            self.val_loader = DataLoader(self.val_dataset, batch_size=batch_size, shuffle=False)
+        self.train_dataset = CustomDataset(train_dataset, transform)
+        self.test_dataset = CustomDataset(test_dataset, test_transform)
+        self.batch_size = batch_size
         
-        self.train_loader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
-        self.val_loader = DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False)
-        self.test_loader = DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False)
+        self.train_loader = DataLoader(self.train_dataset, batch_size=batch_size, shuffle=True)
+        self.test_loader = DataLoader(self.test_dataset, batch_size=batch_size, shuffle=False)
         
-        self.model = Resnet(num_classes=num_classes, model=model_type).to(self.device)
+        self.model = Resnet(num_classes=num_classes, model=model_type, pre_trained=pre_trained).to(self.device)
         
         self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.SGD(self.model.parameters(), momentum=0.9, lr=0.001)
+        self.optimizer = optim.SGD(self.model.parameters(), momentum=0.9, lr=0.01, weight_decay=weight_decay)
 
-    def get_train_val_test_datasets(self, train_dataset):
+    def get_train_val_datasets(self, train_dataset):
         targets = self.get_targets(train_dataset)
-        train_idx, val_idx = train_test_split(range(len(train_dataset)), test_size=0.1, stratify=targets)
+        train_idx, val_idx = train_test_split(range(len(train_dataset)), test_size=0.2, stratify=targets)
         train_dataset = Subset(train_dataset, train_idx)
         val_dataset = Subset(train_dataset, val_idx)
         train_dataset = self.reset_subset_indices(train_dataset)
@@ -50,6 +59,7 @@ class Predictor:
     def train(self, num_epochs):
         best_val_accuracy = 0.0
         self.model.train()
+        lr_scheuler = torch.optim.lr_scheduler.StepLR(self.optimizer, 80, 0.1)
         for epoch in range(num_epochs):
             # Training
             running_loss = 0.0
@@ -64,38 +74,42 @@ class Predictor:
                 
                 running_loss += loss.item()
                 
-            # Validation
-            self.model.eval()
-            val_loss = 0.0
-            val_correct = 0
-            val_total = 0
+            if self.use_validation:
+                # Validation
+                self.model.eval()
+                val_loss = 0.0
+                val_correct = 0
+                val_total = 0
 
-            with torch.no_grad():
-                for val_inputs, val_labels in self.val_loader:
-                    val_inputs, val_labels = val_inputs.to(self.device), val_labels.to(self.device)
-                    val_outputs = self.model(val_inputs)
-                    val_loss += self.criterion(val_outputs, val_labels).item()
-                    _, val_predicted = torch.max(val_outputs.data, 1)
-                    val_total += val_labels.size(0)
-                    val_correct += (val_predicted == val_labels).sum().item()
+                with torch.no_grad():
+                    for val_inputs, val_labels in self.val_loader:
+                        val_inputs, val_labels = val_inputs.to(self.device), val_labels.to(self.device)
+                        val_outputs = self.model(val_inputs)
+                        val_loss += self.criterion(val_outputs, val_labels).item()
+                        _, val_predicted = torch.max(val_outputs.data, 1)
+                        val_total += val_labels.size(0)
+                        val_correct += (val_predicted == val_labels).sum().item()
 
-            val_accuracy = 100 * val_correct / val_total
-            print(f"Validation Accuracy: {val_accuracy:.2f}%")
+                val_accuracy = 100 * val_correct / val_total
+                print(f"Validation Accuracy: {val_accuracy:.2f}%")
+                
 
-            # Save model if it has the best validation accuracy
-            if val_accuracy > best_val_accuracy:
-                best_val_accuracy = val_accuracy
-                torch.save(self.model.state_dict(), 'best_model.pth')
+                # Save model if it has the best validation accuracy
+                if val_accuracy > best_val_accuracy:
+                    best_val_accuracy = val_accuracy
+                    # torch.save(self.model.state_dict(), 'best_model.pth')
+            
+            lr_scheuler.step()
                 
             print(f"Epoch {epoch+1}/{num_epochs}, Loss: {running_loss/len(self.train_loader)}")
-        self.model.load_state_dict(torch.load('best_model.pth'))
+        # self.model.load_state_dict(torch.load('best_model.pth'))
     
     def free_model(self):
         self.model.to('cpu')
         torch.cuda.empty_cache()
     
     def evaluate(self):
-        self.model.load_state_dict(torch.load('best_model.pth'))
+        # self.model.load_state_dict(torch.load('best_model.pth'))
         self.model.eval()
         all_labels = []
         all_predictions = []
--- models/resnet.py
+++ models/resnet.py
@@ -1,22 +1,47 @@
-import torch
 import torch.nn as nn
 from torchvision.models import resnet18, ResNet18_Weights
 from torchvision.models import resnet34, ResNet34_Weights
 from torchvision.models import resnet50, ResNet50_Weights
+import models
+import importlib
+importlib.reload(models)
+
+# import models.preact
+# importlib.import_module('preact', 'models')
+# from models import preact
+# importlib.reload(preact)
+# from preact import PreActResNet18
 
 class Resnet(nn.Module):
-    def __init__(self, num_classes=10, model='resnet18'):
+    def __init__(self, num_classes=10, model='resnet18', pre_trained=True):
         super(Resnet, self).__init__()
         if model == 'resnet18':
-            base_model = resnet18(weights=ResNet18_Weights.DEFAULT)
+            if pre_trained:
+                base_model = resnet18(weights=ResNet18_Weights.DEFAULT)
+            else:
+                base_model = resnet18()
+        elif model == 'preact-resnet18':
+            if pre_trained:
+                raise ValueError('Not supported')
+            else:
+                base_model = models.preact.PreActResNet18()
         elif model == 'resnet34':
-            base_model = resnet34(weights=ResNet34_Weights.DEFAULT)
+            if pre_trained:
+                base_model = resnet34(weights=ResNet34_Weights.DEFAULT)
+            else:
+                base_model = resnet34()
         elif model == 'resnet50':
-            base_model = resnet50(weights=ResNet50_Weights.DEFAULT)
+            if pre_trained:
+                base_model = resnet50(weights=ResNet50_Weights.DEFAULT)
+            else:
+                base_model = resnet50()
         else:
             raise ValueError('Model not supported')
         self.feature_extractor = nn.Sequential(*list(base_model.children())[:-1])
-        self.output = nn.Linear(512, num_classes)
+        if model.__contains__('preact'):
+            self.output = nn.Linear(8192, num_classes)
+        else:
+            self.output = nn.Linear(512, num_classes)
 
     def forward(self, input):
         out = self.feature_extractor(input)
